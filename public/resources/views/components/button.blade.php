26975b521ef9ca1f67dd3df092cfa238c429ecce
kutabarikm <malay.misha@gmail.com>
2023-10-14 15:32:54 +0300

    add posts

---
+++ public/resources/views/components/button.blade.php
@@ -0,0 +1,4 @@
+<button>
+    {{$attributes}}
+    {{$slot}}
+</button>
--- public/routes/web.php
+++ public/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+
+Route::get('posts/{post:slug}', [PostController::class, 'show'])->name('posts');
+
